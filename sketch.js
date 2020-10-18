65247c02a5318fa5efc0b9f1c17bcf377bc86eab
alicia-breinke <leeshia12@gmail.com>
2020-10-18 15:47:49 -0700

    css and html edits

--- sketch.js
+++ sketch.js
@@ -8,23 +8,16 @@ function draw() {
   background(0);
   fill(255, 0, 0);
   ellipse(x,  200, 200);
+}
+function mouseClicked(){
   if (x > 200){
-    x-=2;
+    x-=10;
   }
   else if (x < 200){
-    x+=2;
+    x+=10;
   }
   x+=floor(movedX / 5);
   background(0);
   fill(255, 0, 0);
   ellipse(x, 200, 200);
-}
-function keyTyped() {
-  if (key === 'y'){
-    fill(255, 255, 0);
-  } else if (key === 'g') {
-    fill(204, 255, 0);
-  } else if (key === 'd') {
-    fill(255, 0, 0);
-  }
 }
