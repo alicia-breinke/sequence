961598189e324f09b5bd7a1fa2042efcc777315c
alicia-breinke <leeshia12@gmail.com>
2020-10-18 14:59:57 -0700

    update sketch

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 let x = 400;
 function setup() {
   createCanvas(400, 400);
-  ellipseMode(CENTER);
 }
 
 function draw() {
+  x+=floor(movedX / 5);
+  background(0);
+  fill(255, 0, 0);
+  ellipse(x,  200, 200);
   if (x > 200){
     x-=2;
   }
@@ -14,5 +17,14 @@ function draw() {
   x+=floor(movedX / 5);
   background(0);
   fill(255, 0, 0);
-  ellipse(200, 200, 200, 200);
+  ellipse(x, 200, 200);
+}
+function keyTyped() {
+  if (key === 'y'){
+    fill(255, 255, 0);
+  } else if (key === 'g') {
+    fill(204, 255, 0);
+  } else if (key === 'd') {
+    fill(255, 0, 0);
+  }
 }
